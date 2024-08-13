2d6a20963d0701315341db5094b62f5a5ec83d18
anas <o@gamil.com>
2024-08-13 17:25:39 +0300

    Stripe Logic

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Stripe\Charge;
+use Stripe\Stripe;
+
+// use Stripe;
 
 class StripePaymentController extends Controller
 {
-    //
+    public function stripe()
+    {
+        return view('stripe');
+    }
+
+    public function stripePost(Request $request)
+    {
+        Stripe::setApiKey(env('STRIPE_SECRET'));
+        Charge::create([
+            'amount' => 100*100,
+            'currency' => 'USD',
+            'source' => $request->stripe->token,
+            'description' => 'Test Payment'
+        ]);
+
+        Session::flash('success', 'payment successful');
+
+        return back();
+    }
 }
